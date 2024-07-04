3a8d041166bbffd2a0d90144525abc9beb801761
ArianneAlonso <ariannealonso8@gmail.com>
2024-07-04 15:38:10 -0300

    db

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 
 //base de datos
-const basedatos = [];
+const basedatos = require("./db")
 
 //middleware
 app.use(express.json());
---
+++ db.js
@@ -0,0 +1,9 @@
+const basedatos =
+[{
+    "id": 1,
+    "fullName": "Andres Garcia",
+    "age": 19,
+    "curse": "Anatomia"
+},];
+
+module.exports=basedatos;
